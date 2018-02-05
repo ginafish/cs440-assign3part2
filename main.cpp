0088490d2b19c7f8624baa0b06976055a1f03f65
ginafish <phipps.gina@gmail.com>
2018-02-05 12:04:59 -0800

    Emp list gen did thing.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-#import "dept.h"
-#import "emp.h"
+#import <vector>
+#import <iostream>
+#import <fstream>
+#include <sstream>
 
 struct Emp {
     int eid, age;
     std::string ename;
     double salary;
+
 }
 
 struct Dept {
@@ -14,5 +17,62 @@ struct Dept {
 }
 
 int main() {
-
+    std::vector <Emp> empList = generateEmpList();
 }
+
+std::vector <Emp> generateEmpList() {
+    std::vector <Emp> empList;
+    ofstream empFile;
+    empFile.open("Emp.csv");
+    if(!empFile.is_open()) {
+        std::cout << "Was expecting an 'Emp.csv' file.";
+        throw;
+    }
+    std::string curLine;
+    while(getline(empFile, curLine)) {
+        if(curLine == '\n') {
+            cout << "\n\tEnd of Emp file.\n";
+            break;
+        }
+        std::cout << "[line] " << curLine << "\n";
+        empList.push_back(parseEmp(curLine));
+    }
+}
+
+Emp parseEmp(std::string empLine) {
+    Emp tempEmpObj;
+    std::string tempParse = "";
+    stringstream tempStrStrm(empLine);
+    
+    //get eid
+    getline(tempStrStrm, tempParse, ',');
+    tempParse = trimQuotation(tempParse);
+    std::cout << "Parsed eid: " << tempParse;
+    tempEmpObj.eid = std::stoi(tempParse);
+
+    //get ename
+    getline(tempStrStrm, tempParse, ',');
+    tempParse = trimQuotation(tempParse);
+    std::cout << "Parsed ename: " << tempParse;
+    tempEmpObj.ename = tempParse;
+
+    //get age
+    getline(tempStrStrm, tempParse, ',');
+    tempParse = trimQuotation(tempParse);
+    std::cout << "Parsed age: " << tempParse;
+    tempEmpObj.age = std::stoi(tempParse);
+
+    //get salary
+    getline(tempStrStrm, tempParse, '\n');
+    tempParse = trimQuotation(tempParse);
+    std::cout << "Parsed salary: " << tempParse;
+    tempEmpObj.salary = std::stof(tempParse);
+
+    return tempEmpObj;
+}
+
+std::string trimQuotation(std::string str) {
+    return str.substr(1, str.size() - 2);
+}
+
+
